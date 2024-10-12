791af09bae3073e511615ec2559143db4e70c269
robert18x <bookwolf16@gmail.com>
2024-10-12 09:24:15 +0000

    Added devcontainer config

---
+++ .devcontainer/prepare_env.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y python3 python3-pip gcc-14 g++-14 ninja-build
+pip3 install conan==1.65 cmake --break-system-packages
+
+conan config init
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan profile update settings.compiler.version=14 default
+conan profile update settings.build_type=Debug default
+
