b038dbeb671bc726810e0d331b0258ec2616bb52
robert18x <bookwolf16@gmail.com>
2024-12-16 01:00:46 +0000

    Added getAndErase method to SynchronizedMap template class

--- scaf/SynchronizedMap.h
+++ scaf/SynchronizedMap.h
@@ -6,13 +6,24 @@
 
 namespace scaf {
 
-template <typename Key, typename Value>
+template <typename Key, typename Value, template<typename, typename> typename UnderlyingType = std::map>
 class SynchronizedMap {
 public:
     constexpr std::optional<Value> get(const Key& key) {
         std::scoped_lock guard(accessMutex);
         if (auto it = activeConversations.find(key); it != activeConversations.end()){
             return it->second;
+        } else {
+            return std::nullopt;
+        }
+    }
+
+    constexpr std::optional<Value> getAndErase(const Key& key) {
+        std::scoped_lock guard(accessMutex);
+        if (auto it = activeConversations.find(key); it != activeConversations.end()){
+            auto tmp = std::move(it->second);
+            activeConversations.erase(it);
+            return tmp;
         } else {
             return std::nullopt;
         }
@@ -30,7 +41,7 @@ public:
     }
 
 private:
-    std::map<Key, Value> activeConversations;
+    UnderlyingType<Key, Value> activeConversations;
     std::mutex accessMutex;
 };
     
